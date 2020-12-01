3720a6cec6c3a5ed235a40f3fb414db1ae834550
Mirko <mirko.rinelli@gmail.com>
2020-12-01 17:58:41 +0100

    PC generated random number

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,10 @@
-//Un alert espone 5 numeri casuali diversi.
+$(document).ready(function() {
+  //Un paragrafo espone 5 numeri casuali diversi.
+  $("p span").each(function(index, el) {
+    $(this).text(Math.floor(Number(Math.random() * 100) + 1));
+  });
 
-//Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
+  //Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
 
-//Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+  //Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+});
