98d6ba1a242b87aec4a233c36b5eebd98db716cd
Mirko <mirko.rinelli@gmail.com>
2020-12-01 19:41:32 +0100

    Time out fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,9 +5,24 @@ $(document).ready(function() {
   });
 
   //Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
-  setTimeout(function() {
+
+  setTimeout(timeOut, 3000);
+
+  function timeOut() {
     $("p span").hide();
-  }, 10000);
+  }
+
+  setTimeout(userNumbers, 4000)
+
+  function userNumbers() {
+    var userNumber2 = Number(prompt("Inserisci il 1° numero"));
+    var userNumber3 = Number(prompt("Inserisci il 2° numero"));
+    var userNumber4 = Number(prompt("Inserisci il 3° numero"));
+    var userNumber1 = Number(prompt("Inserisci il 4° numero"));
+    var userNumber5 = Number(prompt("Inserisci il 5° numero"));
+  }
 
   //Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+
+
 });
