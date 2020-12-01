1b4e9f1770585665ac19591b2e0c638b591cc8cb
Mirko <mirko.rinelli@gmail.com>
2020-12-01 22:11:07 +0100

    Don't find solution

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,25 @@
 $(document).ready(function() {
   //Un paragrafo espone 5 numeri casuali diversi.
-  $("p span").each(function(index, el) {
-    $(this).text(Math.floor(Number(Math.random() * 100) + 1));
-  });
+  var pcNumber = [];
+  for (i = 0; i < 5; i++) {
+    var randomNumber = Math.floor(Math.random() * 100); + 1;
+    pcNumber.push(randomNumber);
+    console.log(pcNumber);
+    document.getElementById("list_number").innerHTML = pcNumber;
+  }
 
   //Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
 
   setTimeout(timeOut, 3000);
 
   function timeOut() {
-    $("p span").hide();
+    $("span").hide();
   }
 
-  setTimeout(userNumbers, 4000)
 
-  function userNumbers() {
-    var userNumber2 = Number(prompt("Inserisci il 1° numero"));
-    var userNumber3 = Number(prompt("Inserisci il 2° numero"));
-    var userNumber4 = Number(prompt("Inserisci il 3° numero"));
-    var userNumber1 = Number(prompt("Inserisci il 4° numero"));
-    var userNumber5 = Number(prompt("Inserisci il 5° numero"));
+  for (i = 0; i < 5; i++) {
+    var requestNumber = Number(prompt("Inserisci i numeri visualizzari precedentemente"));
   }
 
   //Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
-
-
 });
